04e7e2c64f6ac23c49bd9a97068152f1155cdb10
a23hugomg <a23hugomg@iessanclemente.net>
2024-10-09 14:46:17 +0200

    Todo menos footer

--- www/ProjectoPHP/pages/index.php
+++ www/ProjectoPHP/pages/index.php
@@ -1,42 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FitTrack</title>
-    <style></style>
-  </head>
-  <body>
-    <div class="header">
-      <div class="logo">
-        <img src="../img/gimnasio (1).png" height="100px"/>
-      </div>
-      <div class="title">
-        <h1>FitTrack</h1>
-      </div>
-      <div class="menu">
-        <a href="./index.php">Home</a>
-        <a href="./log-in/login.php">Log-In</a>
-        <a href="./register/register.php">Register</a>
-      </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>FitTrack</title>
+  <link rel="stylesheet" type="text/css" href="./style.css" media="screen" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <div class="header">
+    <div class="logo">
+      <img src="../img/gimnasio (1).png" height="100px" />
     </div>
-    <div class="body">
-      <div class="subtitle">
-        <h3>Track your workouts, monitor your progress, and stay motivated!</h3>
-      </div>
-      <div class="text">
-        <p>FitTrack is your web application to log your workouts and track your progress.</p>
-      </div>
-      <div class="imgs">
-        <img src="../img/pesas.jpg" height="300px"/>
-        <img src="../img/cintaCorrer.jpg" height="300px"/>
-        <img src="../img/tablas.jpg" height="300px"/>
-      </div>
+    <div class="title">
+      <h1>FitTrack</h1>
     </div>
-    <div class="footer">
-      <div class="box">
-        <p>Footer</p>
-      </div>
+    <div class="menu">
+      <a href="./index.php">Home</a>
+      <a href="./log-in/login.php">Log-In</a>
+      <a href="./register/register.php">Register</a>
     </div>
-  </body>
+  </div>
+  <hr>
+  <div class="body">
+    <div class="subtitle">
+      <h3>Track your workouts, monitor your progress, and stay motivated!</h3>
+    </div>
+    <div class="text">
+      <p>FitTrack is your web application to log your workouts and track your progress.</p>
+    </div>
+    <div class="imgs">
+      <img src="../img/pesas.jpg" />
+      <img src="../img/cintaCorrer.jpg" />
+      <img src="../img/tablas.jpg" />
+    </div>
+  </div>
+  <div class="footer">
+    <div class="box">
+      <p>Footer</p>
+    </div>
+  </div>
+</body>
+
 </html>
