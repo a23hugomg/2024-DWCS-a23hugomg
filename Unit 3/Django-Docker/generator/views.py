33d9e10c1ef7e22d1cf56f4111dac5bf32fc5ed4
a23hugomg <a23hugomg@iessanclemente.net>
2024-11-07 14:35:28 +0100

    Exercise django generate password

--- Unit 3/Django-Docker/generator/views.py
+++ Unit 3/Django-Docker/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -8,6 +9,30 @@ def home(request):
     #return HttpResponse("hola mundo")
     return render(request,"generator/home.html")
 
+def about(request):
+    return render(request,"generator/about.html")
+
 def password(request):
     #return HttpResponse("<h1>Eggs are so tasty</h1>")
-    return render(request,"generator/password.html")
+    thepassword = ''
+    
+    characters = list("abcdefghijklmñopqrstuvwxyz")
+    
+    if request.GET.get("uppercase"):
+        mayus = list()
+        for c in characters:
+            mayus += c.upper()
+        characters.extend(mayus)
+        
+    if request.GET.get("numbers"):
+        characters.extend("0123456789")
+        
+    if request.GET.get("special"):
+        characters.extend("!·$%&/()=?¿`+ḉ,.-^*¨Ç;:_}[]{•· ̣")
+    
+    length = int(request.GET.get("length"))
+    
+    for x in range(length):
+        thepassword += random.choice(characters)
+    
+    return render(request,"generator/password.html",{"password":thepassword})
