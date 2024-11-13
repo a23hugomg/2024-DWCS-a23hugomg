0b094823ec989a918ae3854cb2679bd696e8e606
a23hugomg <a23hugomg@iessanclemente.net>
2024-11-13 11:04:26 +0100

    Personal Portfolio

--- Unit 3/personalPortfolio/portfolio/views.py
+++ Unit 3/personalPortfolio/portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Proyect
 
 # Create your views here.
+def home(request):
+    projects = Proyect.objects.all()
+    return render(request, 'portfolio/home.html', {'projects':projects})
