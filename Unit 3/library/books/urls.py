b169445934aaf6ad8758cb506c1a2d1265cdac69
a23hugomg <a23hugomg@iessanclemente.net>
2024-11-22 21:44:29 +0100

    Funciona todo

--- Unit 3/library/books/urls.py
+++ Unit 3/library/books/urls.py
@@ -1,8 +1,8 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+from . import views
 
-# app_name = 'book'
+app_name = 'book'  # Define el namespace
 
-# urlpatterns = [
-#     path('<int:book_id>/', views.book_details, name='detail')
-# ] 
+urlpatterns = [
+    path('<int:book_id>/', views.book_detail, name='detail'),  # Ruta para detalle del libro
+]
--- Unit 3/library/books/views.py
+++ Unit 3/library/books/views.py
@@ -6,7 +6,6 @@ def home(request):
     books = Book.objects.all()
     return render(request, 'books/home.html', {'books':books})
 
-# def detail(request, book_id):
-#     book = get_object_or_404(Book, pk=book_id)
-
-#     return render(request, 'books/book_details.html',{'book':book})
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'books/book_detail.html', {'book': book})
