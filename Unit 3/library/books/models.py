87f2056aea06e0e5d2254b4fda91e344afdc7e70
a23hugomg <a23hugomg@iessanclemente.net>
2024-11-22 13:45:32 +0100

    Books page

--- Unit 3/library/books/models.py
+++ Unit 3/library/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='books/images/')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title+f" ({self.id})"
--- Unit 3/library/books/views.py
+++ Unit 3/library/books/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-#from models  import Project
+from django.shortcuts import render, get_list_or_404
+from .models  import Book
 
 # Create your views here.
 def home(request):
-    return render(request, 'books/home.html')
+    books = Book.objects.all()
+    return render(request, 'books/home.html', {'books':books})
+
+def book_details(request):
+    book = get_object_or_404(Book, pk=book_id)
+
+    return render(request, 'books/book_details.html',{'book':book})
