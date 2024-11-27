44eb36b3222787f7c0f113442babd04e8d5b2a08
a23hugomg <a23hugomg@iessanclemente.net>
2024-11-27 17:01:18 +0100

    Dictionaries

--- Unit 3/python/Task 3.15.  Dictionaries II.py
+++ Unit 3/python/Task 3.15.  Dictionaries II.py
@@ -2,6 +2,24 @@ import json
 
 with open('catalog.json','r') as file:
     diccionario = json.load(file)
-    
-for k,v in diccionario.values():
-    print(k, v)
+
+# print(isinstance(diccionario, dict))
+
+# Diccionario entero
+for key, value in diccionario.items():
+    if isinstance(value, dict):
+        print(f"{key}:")
+        for subkey, subvalue in value.items():
+            if isinstance(subvalue, list):
+                print(f"  {subkey}:")
+                for item in subvalue:
+                    print("    -")
+                    for item_key, item_value in item.items():
+                        print(f"      {item_key}: {item_value}")
+            else:
+                print(f"  {subkey}: {subvalue}")
+
+
+# Titulo de los libros
+# for book in diccionario['catalog']['book']:
+#     print(f"{book['title']}")
