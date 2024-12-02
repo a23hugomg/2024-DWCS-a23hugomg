59fb28229617363f562f26398636ac67c3de3784
a23hugomg <a23hugomg@iessanclemente.net>
2024-12-02 10:00:31 +0000

    Python anywhere

--- Unit 3/personalPortfolio/personalPortfolio/settings.py
+++ Unit 3/personalPortfolio/personalPortfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dq1$y*!k78_4p3_bejr+vfjw2-&qcwp%zdmk+s^xg(_dcj$=z7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['a23hugomg.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
