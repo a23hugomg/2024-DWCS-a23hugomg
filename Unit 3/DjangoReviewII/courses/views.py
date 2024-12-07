fb1fce2d1267fb1e29b02da2e2a849cf00723e2a
a23hugomg <a23hugomg@iessanclemente.net>
2024-12-07 18:03:22 +0100

    Home, courses page

--- Unit 3/DjangoReviewII/courses/views.py
+++ Unit 3/DjangoReviewII/courses/views.py
@@ -3,5 +3,8 @@ from .models import Courses
 
 # Create your views here.
 def home(request):
+    return render(request, 'courses/home.html')
+
+def courses(request):
     courses = Courses.objects.all()
-    return render(request, 'courses/home.html', {'courses':courses})
+    return render(request, 'courses/courses.html', {'courses':courses})
