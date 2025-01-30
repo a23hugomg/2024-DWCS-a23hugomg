e979c100b540d8e209b2907aabfb29986451cd0f
a23hugomg <a23hugomg@iessanclemente.net>
2025-01-30 14:31:50 +0100

    Erro views

--- Unit 4/Feedback/reviews/urls.py
+++ Unit 4/Feedback/reviews/urls.py
@@ -1,9 +1,8 @@
+from django.urls import path
 
-from django.contrib import admin
-from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.review),
-    path('thank_you', views.thank_you)
+     path("", views.ReviewView.as_view()),
+     path("thank-you", views.thank_you)
 ]
--- Unit 4/Feedback/reviews/views.py
+++ Unit 4/Feedback/reviews/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import ReviewForm
+from .models import Review
+from django.views import View
 
 # Create your views here.
+class ReviewView(View):
+    def get(self,request):
+        form = ReviewForm(request.POST)
 
-def review (request):
-    if request.method == 'POST':
-        entered_username = request.POST["username"]
-        print(entered_username)
-        return HttpResponseRedirect("/thank_you")
-    return render(request,"reviews/review.html")
+        if form.is_valid():
+            form.save()
+            return render("reviews/review.html",{"form": form})
+    def post(self,request):
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank-you")
+        
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
