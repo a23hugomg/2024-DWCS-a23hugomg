4361d0c33230a31d1f9f889d82cd790dd9cc0c86
a23hugomg <a23hugomg@iessanclemente.net>
2025-02-13 14:34:15 +0100

    Tarea

--- Unit 4/Blog/blog/urls.py
+++ Unit 4/Blog/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path("", views.Index.as_view()),
     path("all-posts/",views.PostList.as_view(), name="all_posts"),
-    path("<slug:slug>",views.PostDetail.as_view(), name="blog_detail")
+    path("<slug:slug>",views.PostDetail.as_view(), name="blog_detail"),
+    path("read-later/",views.ReadLater.as_view(), name="read-later")
 ]
--- Unit 4/Blog/blog/views.py
+++ Unit 4/Blog/blog/views.py
@@ -47,7 +47,8 @@ class PostDetail(View):
         context = {
             "post":post,
             "post_tags": post.tag.all(),
-            "comment_form":CommentForm()
+            "comment_form":CommentForm(),
+            "comments": post.comments.all().order_by("-id")
         }
         return render(request, "blog/blog_detail.html",context)
 
@@ -74,4 +75,30 @@ class PostDetail(View):
     #     context["post_tags"] = tags
     #     context["comment_form"] = CommentForm
     #     return context
-    
+
+class ReadLater(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+        if stored_posts is None or len(stored_posts) == 0:
+            context["stored_posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+            
+        return render(request, "blog/stored_posts.html", context)
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is None:
+            stored_posts = []
+        post_id = int(request.POST["post_id"])
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+            
+        request.session["stored_posts"] = stored_posts
+        return HttpResponseRedirect("/")
