024601759ebcfa73c45a8cbecc78b0583cba49a2
a23hugomg <a23hugomg@iessanclemente.net>
2025-02-19 17:56:15 +0100

    models

--- Unit 5/FinalProject/library/books/models.py
+++ Unit 5/FinalProject/library/books/models.py
@@ -1,21 +1,27 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField()
+    image = models.ImageField(upload_to='authors/images/')
+    readers = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=250)
     image = models.ImageField(upload_to='books/images/')
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     publication_date = models.DateField()
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    authors = models.ManyToManyField(Author)
 
     def __str__(self):
         return self.title+f" ({self.id})"
     
-class Author(models.Model):
-    name = models.CharField(max_length=255)
-    bio = models.TextField()
-    image = models.ImageField(upload_to='authors/images/')
-
-    def __str__(self):
-        return self.name
