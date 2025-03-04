56d15ee4dbcb0e749737e1ac085e51fb90def1f0
a23hugomg <a23hugomg@iessanclemente.net>
2025-03-04 14:41:54 +0100

    exercise

--- Unit 5/Review1/student/models.py
+++ Unit 5/Review1/student/models.py
@@ -11,12 +11,12 @@ class Degree(models.Model):
     years = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(4)])
     
     def __str__(self):
-        return ( self.id ) + self.name
-    
+        return str(self.id) + " - " + self.name
+
 class Student(models.Model):
     name_surname = models.CharField(max_length=250)
     picture = models.ImageField()
-    age = models.IntegerField(validators=[MinValueValidator(1970),MaxValueValidator(2010)])
+    year = models.IntegerField(validators=[MinValueValidator(1970),MaxValueValidator(2010)])
     slug = models.SlugField(default="", null=False, db_index=True, unique=True)
     finished_degree = models.BooleanField(default=False)
     degree = models.ForeignKey(Degree, related_name="fkstudents", on_delete=models.CASCADE, null=True)
